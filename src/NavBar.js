8ade2992a8c933933614cf717c1edb90070eb824
Lior Karamany <lior.karamany@gmail.com>
2021-07-27 12:52:10 +0300

    Added cool font to logo.

--- src/NavBar.js
+++ src/NavBar.js
@@ -16,6 +16,7 @@ import MailIcon from "@material-ui/icons/Mail";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import MoreIcon from "@material-ui/icons/MoreVert";
 import { Link } from "react-router-dom";
+import "./fonts.css";
 
 const useStyles = makeStyles((theme) => ({
   grow: {
@@ -29,6 +30,7 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.up("sm")]: {
       display: "block",
     },
+    fontFamily: ["Lobster", "cursive"],
   },
   search: {
     position: "relative",
@@ -173,7 +175,7 @@ export default function NavBar() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography className={classes.title} variant="h6" noWrap>
+          <Typography className={classes.title} variant="h4" noWrap>
             Communit 🌐
           </Typography>
           <div className={classes.search}>
